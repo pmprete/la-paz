bb1dacf07abafee193066ce7ff0e3a2b1aefc11e
Comandante Korsho <martinprete@gmail.com>
2014-02-04 21:11:19 -0300

    Dashbord con letra vieja. Ajustado para mobile.

--- application/views/layout_main.php
+++ application/views/layout_main.php
@@ -32,11 +32,12 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="index.html">Sistema de Cobranza - Municipalidad de La Paz</a>
+        <a class="navbar-brand mini-title" href="index.html">Sistema de Cobranza</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <a class="navbar-brand" href="index.html">Sistema de Cobranza - Municipalidad de La Paz</a>
         <ul class="nav navbar-nav side-nav">
             <li class="active"><a href="index.html"><i class="fa fa-dashboard"></i> Escritorio</a></li>
             <li><a href="#"><i class="fa fa-bar-chart-o"></i> Charts</a></li>
